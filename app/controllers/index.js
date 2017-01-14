a2cc19df208609ccd47b81e17f3c704a2e1ad900
pgsoto <dex.pgsoto@gmail.com>
2017-01-14 17:57:59 -0300

    implemento items

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,6 +2,15 @@ angular.module('app', [])
 
 .controller('controlador', function($scope,$http){
 	
+	$http({method:'get', url:'http://localhost:8000/all'})
+	.then(function(data){
+		$scope.items = data.data;
+		console.log(data);
+	}, function(err){
+		console.log(err);
+	})
+
+
 	$scope.agregar = function(nombre){
 		var data = {
 			name: nombre
